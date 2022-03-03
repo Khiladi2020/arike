8b6e13ae88ee1c8cafe5f1ad91de3355315de307
Ravinder Singh <ravindermaan0123@gmail.com>
2022-03-04 01:36:38 +0530

    added application namespace

--- app_admin/urls.py
+++ app_admin/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name="app_admin"
 urlpatterns = [
     path('', views.index, name="index")
 ]
--- patient/urls.py
+++ patient/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "patient"
 urlpatterns = [
     path('', views.PatientListView.as_view(), name="patient_list"),
     path('<int:pk>/', views.PatientDetailView.as_view(), name="patient_detail"),
