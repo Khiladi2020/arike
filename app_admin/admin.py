112bdfaf316582711ec056bf255c77420a37b60d
Ravinder Singh <ravindermaan0123@gmail.com>
2022-03-03 12:40:05 +0530

    updated admin file for app_admin app

--- app_admin/admin.py
+++ app_admin/admin.py
@@ -3,4 +3,4 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import AppUser, AppUserManager
 # Register your models here.
-admin.register(AppUser, UserAdmin)
+admin.site.register(AppUser)
