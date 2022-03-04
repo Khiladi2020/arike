16691e323c54140b36af405a4ca6dccb7e6f79df
Ravinder Singh <ravindermaan0123@gmail.com>
2022-03-04 19:56:46 +0530

    created Create page and updated few other pages

--- app_admin/urls.py
+++ app_admin/urls.py
@@ -6,5 +6,6 @@ app_name="app_admin"
 urlpatterns = [
     path('', views.index, name="index"),
     path('facility/',views.FacilityListView.as_view(), name="facility_list"),
-    path('facility/<int:pk>',views.FacilityDetailView.as_view(), name="facility_detail")
+    path('facility/<int:pk>',views.FacilityDetailView.as_view(), name="facility_detail"),
+    path('facility/create/',views.FacilityCreateView.as_view(), name="facility_form"),
 ]
--- app_admin/views.py
+++ app_admin/views.py
@@ -5,15 +5,26 @@ from django.views import generic
 from .models import Facility
 
 # Create your views here.
+
+
 def home_page(request):
-    return render(request,'base.html',{})
+    return render(request, 'base.html', {})
     # return HttpResponse("coolie gyzs")
-    
+
+
 def index(request):
-    return render(request,'base.html',{})
+    return render(request, 'base.html', {})
+
 
 class FacilityListView(generic.ListView):
     model = Facility
 
+
 class FacilityDetailView(generic.DetailView):
     model = Facility
+
+
+class FacilityCreateView(generic.CreateView):
+    model = Facility
+    success_url = "/admin/facility/"
+    fields = ['kind', 'name', 'address', 'pincode', 'phone', 'ward']
