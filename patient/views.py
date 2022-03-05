d53e5779e48542f9a99e9b800e3de024af9199e5
Ravinder Singh <ravindermaan0123@gmail.com>
2022-03-05 22:17:25 +0530

    added login required mixin to patient app

--- patient/views.py
+++ patient/views.py
@@ -1,31 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Patient
 
 # Create your views here.
 
 
-class PatientListView(generic.ListView):
+class PatientListView(LoginRequiredMixin, generic.ListView):
     # commented this code as thease are deafult values itself
     # template_name = "patient/patient_list.html"
     # context_object_name = "patient_list"
     model = Patient
 
 
-class PatientDetailView(generic.DetailView):
+class PatientDetailView(LoginRequiredMixin, generic.DetailView):
     model = Patient
 
 
-class PatientCreateView(generic.CreateView):
+class PatientCreateView(LoginRequiredMixin, generic.CreateView):
     model = Patient
     success_url = "/patient/"
     fields = ["full_name", "date_of_birth", "address",
               "landmark", "phone", "gender", "emergency_phone_number", "ward", "facility"]
 
 
-class PatientUpdateView(generic.UpdateView):
+class PatientUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Patient
     template_name = "patient/patient_update.html"
     success_url = "/patient/"
