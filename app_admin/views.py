23b7b4c842ad59d199f1fd3cd77e407184347f8e
Ravinder Singh <ravindermaan0123@gmail.com>
2022-03-05 19:28:01 +0530

    added mixin to allow only admins

--- app_admin/views.py
+++ app_admin/views.py
@@ -1,10 +1,8 @@
-from re import L, template
-import django
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 
 from app_admin.admin import UserChangeForm, UserCreationForm
 
@@ -12,70 +10,85 @@ from .models import AppUser, Facility
 
 # Create your views here.
 
+# Home
 
 def home_page(request):
     return render(request, 'app_admin/home.html', {})
-    # return HttpResponse("coolie gyzs")
+
+# Profile
+class ProfilePageView(LoginRequiredMixin, generic.TemplateView):
+    template_name = "app_admin/profile.html"
+
+# Autorization
 
 
-def login_page(request):
-    return render(request, 'login.html', {})
+class AdminAccessRequired(LoginRequiredMixin, PermissionRequiredMixin):
+    def has_permission(self):
+        if self.request.user.role == "district_admin":
+            return True
+        return False
 
+    def get_permission_denied_message(self):
+        return "Only District Admin's can access this resource."
 
 # FACILITY
-class FacilityListView(LoginRequiredMixin,generic.ListView):
+
+
+class FacilityListView(AdminAccessRequired, generic.ListView):
     model = Facility
 
 
-class FacilityDetailView(generic.DetailView):
+class FacilityDetailView(AdminAccessRequired, generic.DetailView):
     model = Facility
 
 
-class FacilityCreateView(generic.CreateView):
+class FacilityCreateView(AdminAccessRequired, generic.CreateView):
     model = Facility
     success_url = "/admin/facility/"
     fields = ['kind', 'name', 'address', 'pincode', 'phone', 'ward']
 
 
-class FacilityUpdateView(generic.UpdateView):
+class FacilityUpdateView(AdminAccessRequired, generic.UpdateView):
     model = Facility
     template_name = "app_admin/facility_update.html"
     success_url = "/admin/facility/"
     fields = ['kind', 'name', 'address', 'pincode', 'phone', 'ward']
 
 
-class FacilityDeleteView(generic.DeleteView):
+class FacilityDeleteView(AdminAccessRequired, generic.DeleteView):
     model = Facility
     success_url = "/admin/facility/"
 
 # USERS
 
 
-class UserListView(generic.ListView):
+class UserListView(AdminAccessRequired, generic.ListView):
     model = AppUser
 
 
-class UserDetailView(generic.DetailView):
+class UserDetailView(AdminAccessRequired, generic.DetailView):
     model = AppUser
 
 
-class UserCreateView(generic.CreateView):
+class UserCreateView(AdminAccessRequired, generic.CreateView):
     form_class = UserCreationForm
     template_name = "app_admin/appuser_form.html"
     success_url = "/admin/user/"
 
 
-class UserUpdateView(generic.UpdateView):
+class UserUpdateView(AdminAccessRequired, generic.UpdateView):
     model = AppUser
     form_class = UserChangeForm
     template_name = "app_admin/appuser_update.html"
     success_url = "/admin/user/"
 
 
-class UserDeleteView(generic.DeleteView):
+class UserDeleteView(AdminAccessRequired, generic.DeleteView):
     model = AppUser
     success_url = "/admin/user/"
 
 # Auth Views
+
+
 class AuthLoginView(LoginView):
     template_name = "login.html"
