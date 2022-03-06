fb8ef7d11b171416314c328d7be2dcce085b7acc
Ravinder Singh <ravindermaan0123@gmail.com>
2022-03-07 03:15:06 +0530

    added visit models to patient admin

--- patient/admin.py
+++ patient/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 from .models import (Disease, FamilyDetail, Patient, PatientDisease, Treatment,
-                     TreatmentNotes)
+                     TreatmentNotes, Visit, VisitSchedule)
 
 # Register your models here.
 admin.site.register([Patient, FamilyDetail, Disease,
-                    PatientDisease, Treatment, TreatmentNotes])
+                    PatientDisease, Treatment, TreatmentNotes, Visit, VisitSchedule])
